a87dc75c8da9b01a8807b8911b5cc098139160d5
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2024-08-27 12:55:28 +0200

    fix: log_error fixed

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Log;
 if (!function_exists('log_error')) {
     function log_error($e)
     {
-        Log::error($e->getMessage());
-        Log::error($e->getTraceAsString());
+        if ($e instanceof Exception) {
+            Log::error($e->getMessage());
+            Log::error($e->getTraceAsString());
+        } else {
+            Log::error($e);
+        }
     }
 }
