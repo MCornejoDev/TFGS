40e6065e6723e9def3a047f5ebea5749a0dffd12
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2024-09-03 10:05:44 +0200

    fix: The inputs use a wire.model without live

--- resources/views/livewire/character/create.blade.php
+++ resources/views/livewire/character/create.blade.php
@@ -33,14 +33,29 @@
     </div>
 
     <div class="grid items-center grid-cols-2 gap-4">
+        <x-livewire.forms.input :label="__('characters.actions.create.form.strength')" :placeholder="__('characters.actions.create.form.placeholder.strength')" type="number" model="form.strength" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.dexterity')" :placeholder="__('characters.actions.create.form.placeholder.dexterity')" type="number" model="form.dexterity" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.constitution')" :placeholder="__('characters.actions.create.form.placeholder.constitution')" type="number" model="form.constitution" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.intelligence')" :placeholder="__('characters.actions.create.form.placeholder.intelligence')" type="number" model="form.intelligence" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.wisdom')" :placeholder="__('characters.actions.create.form.placeholder.wisdom')" type="number" model="form.wisdom" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.charisma')" :placeholder="__('characters.actions.create.form.placeholder.charisma')" type="number" model="form.charisma" />
+    </div>
+
+    <div class="grid items-center grid-cols-2 gap-4">
         @for ($i = 0; $i <= 3; $i++)
             <x-livewire.forms.input :label="__('characters.actions.create.form.skill', ['number' => $i + 1])" :placeholder="__('characters.actions.create.form.placeholder.skill')" model="form.skills.{{ $i }}" />
         @endfor
     </div>
 
+    <div class="grid items-center grid-cols-2 gap-4">
+        <x-livewire.forms.input :label="__('characters.actions.create.form.height')" :placeholder="__('characters.actions.create.form.placeholder.height')" type="number" model="form.height" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.weight')" :placeholder="__('characters.actions.create.form.placeholder.weight')" type="number" model="form.weight" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.health')" :placeholder="__('characters.actions.create.form.placeholder.health')" type="number" model="form.health" />
+        <x-livewire.forms.input :label="__('characters.actions.create.form.level')" :placeholder="__('characters.actions.create.form.placeholder.level')" type="number" model="form.level" />
+    </div>
+
     <textarea class="placeholder:font-bold textarea textarea-bordered bg-base-300"
-        placeholder="{{ __('characters.actions.create.form.placeholder.personality') }}"
-        wire:model.live="form.personality"></textarea>
+        placeholder="{{ __('characters.actions.create.form.placeholder.personality') }}" wire:model="form.personality"></textarea>
 
     <x-livewire.forms.button :title="__('characters.actions.create.btn')" :action="'create'" />
 </div>
