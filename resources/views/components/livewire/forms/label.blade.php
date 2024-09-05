975eaf5809c5379f36003652d0f595e4768850e4
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2024-09-05 11:05:12 +0200

    fix: Now the label component can be used without value

--- resources/views/components/livewire/forms/label.blade.php
+++ resources/views/components/livewire/forms/label.blade.php
@@ -3,11 +3,17 @@
     'value' => null,
 ])
 
-<div class="space-y-8 text-sm font-medium text-base-content">
-    <span class="block mb-2 text-sm font-medium text-base-content">
+@if ($value)
+    <div class="space-y-8 text-sm font-medium text-base-content">
+        <span class="block mb-2 text-sm font-medium text-base-content">
+            {{ $label }}
+        </span>
+        <span class="text-sm cursor-default">
+            {{ $value }}
+        </span>
+    </div>
+@else
+    <span class="block text-sm font-medium text-base-content">
         {{ $label }}
     </span>
-    <span class="text-sm cursor-default">
-        {{ $value }}
-    </span>
-</div>
+@endif
