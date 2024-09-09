5abe9dd9caa9891705e8491d004857223795a5d6
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2024-09-09 12:36:22 +0200

    feat: Now, the user has a timezone assigned

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 
 if (!function_exists('log_error')) {
@@ -18,5 +19,12 @@ if (!function_exists('snake_lower')) {
     function snake_lower($value)
     {
         return Str::snake(Str::lower($value));
+    }
+}
+
+if (!function_exists('parse_date')) {
+    function parse_date($value, ?string $format = null, ?string $tz = null)
+    {
+        return Carbon::parse($value, $tz)->format($format);
     }
 }
