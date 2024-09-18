5a84ab8a89f4505e445f8d30cc0e0ccd50ab234b
MCornejoDev <miguel.cornejo.manzanares@gmail.com>
2024-09-18 10:56:39 +0200

    fix: Try diceRoller

--- resources/views/livewire/tool/index.blade.php
+++ resources/views/livewire/tool/index.blade.php
@@ -1,4 +1,12 @@
-<div x-data="diceRoller()">
-    <p>Resultado: <span x-text="rollResult"></span></p>
-    <button @click="rollDice">Tirar Dados</button>
+<div class="p-4 border rounded-md shadow-lg h-96 border-base-content/30 bg-base-100">
+    <div x-data="diceRoller2" class="w-52">
+        <x-livewire.forms.select :items="$this->toolTypes" title="{{ __('tools.dice_roller.title') }}"
+            placeholder="{{ __('tools.dice_roller.placeholder') }}" model="die" optionId="id" optionLabel="name" />
+
+        <div class="ui_dice">
+            <div class="platform" :class="{ 'stop': !isPlaying, 'playing': isPlaying }" x-ref="platform">
+                <x-livewire.dices.six />
+            </div>
+        </div>
+    </div>
 </div>
